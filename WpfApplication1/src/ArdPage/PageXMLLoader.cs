64916238e3884ac00d077271dc88c7218ca86463
alekseyIsakin <karahunforever@gmail.com>
2021-04-13 08:26:10 +0800

    Использование using при открытии файла

--- WpfApplication1/src/ArdPage/PageXMLLoader.cs
+++ WpfApplication1/src/ArdPage/PageXMLLoader.cs
@@ -18,17 +18,19 @@ namespace ArdClock.ArdPage
             List<APage> pageList = new List<APage>();
 
             XmlSerializer serializer = new XmlSerializer(typeof(List<APage>));
-            try
+
+            using (var sw = new System.IO.StreamReader(fileName))
             {
-                System.IO.StreamReader sw = new System.IO.StreamReader(fileName);
-                pageList = (List<APage>)serializer.Deserialize(sw);
-                sw.Close();
-            }
-            catch (System.IO.FileNotFoundException e)
-            {
-                MessageBox.Show("В директории не неайден существующий файл с настройками, поэтому будет создан новый\n" + e.Source);
-                pageList.Add(new APage());
-                WritePageListToXML(pageList, fileName); 
+                try
+                {
+                    pageList = (List<APage>)serializer.Deserialize(sw);
+                }
+                catch (System.IO.FileNotFoundException e)
+                {
+                    MessageBox.Show("В директории не неайден существующий файл с настройками, поэтому будет создан новый\n" + e.Source);
+                    pageList.Add(new APage());
+                    WritePageListToXML(pageList, fileName);
+                }
             }
 
             return pageList;
--- WpfApplication1/src/ArdPage/PageXMLWriter.cs
+++ WpfApplication1/src/ArdPage/PageXMLWriter.cs
@@ -16,15 +16,13 @@ namespace ArdClock.ArdPage
         static public void WritePageListToXML(List<APage> pageList, string fileName)
         {
             XmlSerializer serializer = new XmlSerializer(typeof(List<APage>));
-            System.IO.StreamWriter sw = new System.IO.StreamWriter(fileName);
-            try
+
+            using (var sw = new System.IO.StreamWriter(fileName))
             {
-                serializer.Serialize(sw, pageList);
-                sw.Close();
-            }
-            catch 
-            {
-                MessageBox.Show("Не удалось сохранить файл настроек");
+                try
+                { serializer.Serialize(sw, pageList); }
+                catch (Exception e)
+                { MessageBox.Show("Не удалось сохранить файл настроек" + e.Message); }
             }
         }
     }
